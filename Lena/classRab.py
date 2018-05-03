3fb8991fe9eca4959fc37c8fdb25e19950e1f521
Uzer612 <uzer612@ya.ru>
2018-05-03 21:28:37 +0300

    test ajax

--- Lena/classRab.py
+++ Lena/classRab.py
@@ -1,16 +1,28 @@
 class Rab:
     def __init__(self, how_many_char, char):
-        self.list_of_number = []
-        self.resultat_clean = []
-        self.resultat_add = []
-    def add(self, how_many_char, char):
-        for i in range(how_many_char):
-            resultat_add = self.list_of_number.append(char)
-    def clean(self, how_many_char, char):
-        while num_of_elenemt > how_many_char:
-            resultat_clean = del self.list_of_number[num_of_elenemt]
-            num_of_elenemt--
-        print(resultat_clean)
+        self.how_many_char = how_many_char
+        self.char = char
+    def add(self, list):
+        list2 = list.copy()
+        if len(list2) < self.how_many_char:
+            while len(list2) < self.how_many_char:
+                list2.append(self.char)
+        print(list2)
+        return list2
+    def clean(self, list):
+        list2 = list[0:self.how_many_char]
+        print(list2)
+        return list2
 
-rab1 = Rab()
-rab1.add(5, 3)
+
+listik = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
+
+rab1 = Rab(5, 3)
+rab1.add(listik)
+rab1.clean(listik)
+
+rab2 = Rab(13, 7)
+rab2.add(listik)
+rab2.clean(listik)
+
+print('listik', listik)
